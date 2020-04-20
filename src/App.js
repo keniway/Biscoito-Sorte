59f89d23c981352f7a0ae695e95ad8a3f912ac93
Ronaldo Linhares <ronaldolinharesrhino@gmail.com>
2020-04-20 09:13:51 -0300

    ciração de props entre varios outras

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import React from 'react';
 
-const BemVindo = (props) => {
-        return(
-            <div>
-                <h2>Chama no problema {props.problem} </h2>
-                <h2>Bem-Vinda(a) {props.king}</h2>
-        </div> 
-        ); }
-function App(){
+const Equipe = (props) => {
+    return( 
+           <div>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade}/>
+           </div>
+); }
+
+const Sobre = (props) => {
     return(
         <div>
-            Olá Mundo!
-            <BemVindo problem="Ronaldo" king="É eu"/>
-            <h1>Curso React</h1>
+            <h2>Olá eu sou o: {props.nome}</h2>
+            <h3>Tenho {props.idade} e sou {props.cargo} deste lindo projeto}</h3>
+            
+            
         </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div>
+        <a href={props.fb}>Facebook</a>
+        <br />
+        <a href={props.ig}>Instagram</a>
+        </div>
+    );
+}
+
+function App(){
+    return(
+       <div>
+           <h1> Conheça nossa equipe:</h1>
+           <Equipe nome="Ronaldo" idade="19" cargo="Desenvolvedor"/>
+           <Social fb="www.google.com" ig="www.youtube.com" />
+       </div>
     );
 }
 
