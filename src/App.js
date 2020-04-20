0ba258973409eb40da0a511db63a46e7516e61e1
Ronaldo Linhares <ronaldolinharesrhino@gmail.com>
2020-04-20 08:16:01 -0300

    Segundo ou primeiro commit apos config iniciais

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BemVindo = (props) => {
+        return(
+            <div>
+                <h2>Chama no problema {props.problem} </h2>
+                <h2>Bem-Vinda(a) {props.king}</h2>
+        </div> 
+        ); }
+function App(){
+    return(
+        <div>
+            Olá Mundo!
+            <BemVindo problem="Ronaldo" king="É eu"/>
+            <h1>Curso React</h1>
+        </div>
+    );
 }
 
 export default App;
